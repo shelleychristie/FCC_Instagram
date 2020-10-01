266dc7f0b41932225cccfc0d3e98b85d730e32db
shelleychristie <57032185+shelleychristie@users.noreply.github.com>
2020-10-01 18:50:23 +0700

    auth
    NPM compiled, set up, user auth made

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Shelley's instagram
                 </div>
 
                 <div class="links">
